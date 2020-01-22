e2910941024407ce4a4aa9e23e25507f261eb405
MacStudent <macstudent@128-IMAC18.local>
2020-01-22 18:49:54 -0500

    Initial commit

---
+++ src/main/java/com/lambton/FirstExample.java
@@ -0,0 +1,51 @@
+package com.lambton;
+
+public class FirstExample
+{
+    public static void main(String[] args)
+    {
+        int i;
+        System.out.println("Welcome to JAVA Programming");
+        //for(i=1;i<=10;i++)
+        //{
+        //  System.out.println("Jyothi Thomas");
+       // }
+        //day 2 code
+        short a =32767;
+        int b;
+
+        b = 200;
+
+        int c = a+b;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(a+b +"sum");
+        System.out.println("short max=" + Short.MAX_VALUE);
+        System.out.println("short min=" + Short.MIN_VALUE);
+
+        long y = 2147483647;
+        System.out.println(y);
+        float f1 = 1000.40F;
+        double f2 = 1000.60D;
+
+        System.out.println("F1="+f1);
+        System.out.println("f2="+f2);
+
+        float f = 10.0F/3;
+        int i3 = (int)(10.0F/3);
+        float i4 =  (float) 10/3;
+        System.out.println(f);
+        System.out.println(i3);
+        System.out.println(i4);
+
+        double x1 = 1_00_00.5_0/10;
+        System.out.println(x1);
+
+
+
+
+
+
+           }
+}
